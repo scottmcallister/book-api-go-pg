f48032bc7843ea5d32363637f32ff005830c0cba
Scott McAllister <scottm@nextopia.com>
2016-02-17 12:42:44 -0500

    forgot to set the port using the environment variable in main.go

--- src/cmd/main/main.go
+++ src/cmd/main/main.go
@@ -58,7 +58,12 @@ func main() {
 		v1.OPTIONS("/users/:id", OptionsUser) // PUT, DELETE
 	}
 
-	r.Run(":8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	r.Run(":" + port)
 }
 
 func GetUsers(c *gin.Context) {
